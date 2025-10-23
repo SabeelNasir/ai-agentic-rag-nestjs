865a9e6702fc2c30b45ba6250bc7bec7eac1a413
SabeelNasir <33089105+SabeelNasir@users.noreply.github.com>
2025-10-23 21:34:48 +0500

    cors enabled for all origins

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from "@nestjs/config";
 import { EnvConfigService } from "./config/env-config.service";
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { ResponseInterceptor } from "./common/interceptors/response-interceptor";
+import cors from "cors";
 
 const configService = new EnvConfigService(new ConfigService());
 const logger = new Logger();
@@ -11,7 +12,12 @@ const logger = new Logger();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: "*", // For development, you can use '*' or specify your React app's local/ngrok URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
